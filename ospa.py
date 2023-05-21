4d681448127e875efaef75baaccb9f402f81d477
Maciej Rozewicz <rozewicz.maciej@gmail.com>
2023-05-21 20:54:53 +0200

    Clean-up

--- ospa.py
+++ ospa.py
@@ -27,21 +27,6 @@ def ospa(x, y, c = 10, p = 1, metric = esm.gw_distance):
 
     return dist
 
-# def ospa(reference, estimation, c = 10, p = 1, metric = esm.gw_distance):
-#     # Get numbers of reference and estimated objects
-#     reference_objects_number = reference.shape[0]
-#     estimation_objects_number = estimation.shape[0]
-#     # Assignment
-#     distances = calculate_distances(reference, estimation, p, metric)
-#     assignment_value = make_assignment(distances)
-
-#     # Metric
-#     cardinality_part = np.power(c, p) * (estimation_objects_number - reference_objects_number)
-#     distance_part = assignment_value
-#     ospa_metric = np.power((distance_part + cardinality_part) / estimation_objects_number, 1.0 / p)
-    
-#     return ospa_metric
-
 def calculate_distances(x, y, p, metric):
     # Get numbers of reference and estimated objects
     n = x.shape[0]
@@ -55,18 +40,3 @@ def calculate_distances(x, y, p, metric):
             distances[reference_index, estimation_index] = np.power(metric(reference_ellipse, estimation_ellipse), p)
 
     return distances
-
-# def make_assignment(distances):
-#     # Get Dimensions
-#     rows_number = distances.shape[0]
-#     cols_number = distances.shape[1]
-#     # Make input graph
-#     input = {}
-#     for row_index in range(rows_number):
-#         row = {}
-#         for column_index in range(cols_number):
-#             row[str(column_index)] = distances[row_index, column_index]
-#         input[str(row_index)] = row
-
-#     assignment_value = algorithm.find_matching(input, matching_type = 'min', return_type = 'total')
-#     return assignment_value
